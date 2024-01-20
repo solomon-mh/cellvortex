1c7433a2ab26fe310f3d93be97badda7fae940d9
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-01-20 08:34:41 +0300

    modfying the addProduct method

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -1,40 +1,38 @@
 "use server";
+
 import { z } from "zod";
 import fs from "fs/promises";
+import { notFound, redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 import prisma from "@/db/db";
-import { redirect } from "next/navigation";
 
-const fileSchema = z.instanceof(File, { message: "required" });
+const fileSchema = z.instanceof(File, { message: "Required" });
 const imageSchema = fileSchema.refine(
   (file) => file.size === 0 || file.type.startsWith("image/")
 );
 
 const addSchema = z.object({
   name: z.string().min(2),
-  description: z.string().min(2),
-  priceInCents: z.coerce.number().positive("The Number must be Positive!"),
-  file: fileSchema.refine((file) => file.size > 0, "required"),
-  image: imageSchema.refine((file) => file.size > 0, "required"),
+  description: z.string().min(5),
+  priceInCents: z.coerce.number().int().min(1),
+  file: fileSchema.refine((file) => file.size > 0, "Required"),
+  image: imageSchema.refine((file) => file.size > 0, "Required"),
 });
 
-export async function addProduct(formData: FormData) {
+export async function addProduct(prevState: unknown, formData: FormData) {
   const result = addSchema.safeParse(Object.fromEntries(formData.entries()));
-  if (result.success === false) {
-    console.log(result.error);
-    return result.error.formErrors;
+  if (!result.success) {
+    return result.error.formErrors.fieldErrors;
   }
-  console.log("result");
 
   const data = result.data;
-  console.log(data);
 
-  // File Path
   await fs.mkdir("products", { recursive: true });
   const filePath = `products/${crypto.randomUUID()}-${data.file.name}`;
   await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()));
-  // Image Path
+
   await fs.mkdir("public/products", { recursive: true });
-  const imagePath = `/products/${crypto.randomUUID()}-${data.file.name}`;
+  const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`;
   await fs.writeFile(
     `public${imagePath}`,
     Buffer.from(await data.image.arrayBuffer())
@@ -42,12 +40,18 @@ export async function addProduct(formData: FormData) {
 
   await prisma.product.create({
     data: {
+      isAvailableForPurchase: false,
       name: data.name,
-      desciption: data.description,
+      description: data.description,
       priceInCents: data.priceInCents,
-      filePath: filePath,
-      imagePath: imagePath,
+      filePath,
+      imagePath,
     },
   });
   redirect("/admin/products");
 }
+
+const editSchema = addSchema.extend({
+  file: fileSchema.optional(),
+  image: imageSchema.optional(),
+});
