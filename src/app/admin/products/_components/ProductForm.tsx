961034b64a2653672311aa64f05c916d3772bfe9
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-01-27 08:46:49 +0300

    modfying productform adding some error handler and also refactoring use-client into another page

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -1,19 +1,35 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@radix-ui/react-label";
 import { useState } from "react";
-import { addProduct } from "../../_actions/products";
+import { addProduct, updateProduct } from "../../_actions/products";
+import { useFormState } from "react-dom";
+import { SubmitButton } from "./ProductSubmitButton";
+import { Product } from "@prisma/client";
+import Image from "next/image";
 
-const ProductForm = () => {
-  const [price, setPrice] = useState<number>();
+const ProductForm = ({ product }: { product?: Product | null }) => {
+  const [error, formAction] = useFormState(
+    product === null || product === undefined
+      ? addProduct
+      : updateProduct.bind(null, product?.id ?? ""),
+    {}
+  );
+  const [price, setPrice] = useState<number | undefined>(product?.priceInCents);
   return (
-    <form action={addProduct} className="space-y-8">
+    <form action={formAction} className="space-y-8">
       <div className="space-y-2">
         <Label htmlFor="name">Name: </Label>
-        <Input type="text" id="name" name="name" required />
+        <Input
+          type="text"
+          id="name"
+          name="name"
+          required
+          defaultValue={product?.name}
+        />
+        {error.name && <div className="text-destructive">{error.name}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="priceInCents">Price-In-Cents: </Label>
@@ -22,13 +38,25 @@ const ProductForm = () => {
           id="priceInCents"
           name="priceInCents"
           required
+          defaultValue={product?.priceInCents}
           onChange={(e) => setPrice(Number(e.target.value) || undefined)}
         />
         <div className="text-muted-foreground">{(price || 0) / 100}$</div>
+        {error.priceInCents && (
+          <div className="text-destructive">{error.priceInCents}</div>
+        )}
       </div>
       <div className="space-y-2">
         <Label htmlFor="description">Description: </Label>
-        <Textarea id="description" name="description" required />
+        <Textarea
+          id="description"
+          name="description"
+          required
+          defaultValue={product?.description}
+        />
+        {error.description && (
+          <div className="text-destructive">{error.description}</div>
+        )}
       </div>
       <div className="space-y-2">
         <label
@@ -42,9 +70,11 @@ const ProductForm = () => {
             type="file"
             id="file"
             name="file"
-            required
+            required={product === null}
             className="block h-fit w-full text-sm text-gray-800 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700"
           />
+          <small className="text-gray-500">{product?.filePath}</small>
+          {error.file && <div className="text-destructive">{error.file}</div>}
         </div>
       </div>
       <div className="space-y-2">
@@ -59,12 +89,23 @@ const ProductForm = () => {
             type="file"
             id="image"
             name="image"
-            required
+            required={product === null}
             className="block h-fit w-full text-sm text-gray-800 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-blue-600 file:text-gray-100"
           />
+          {product && (
+            <div>
+              <Image
+                src={product?.imagePath || ""}
+                width={400}
+                height={400}
+                alt={product?.name || "Product image"}
+              />
+            </div>
+          )}
+          {error.image && <div className="text-destructive">{error.image}</div>}
         </div>
       </div>
-      <Button type="submit">Save</Button>
+      <SubmitButton />
     </form>
   );
 };
