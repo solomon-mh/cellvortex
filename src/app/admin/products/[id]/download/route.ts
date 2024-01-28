3921980dfbd2269abb1d713208a2dbe9ee945554
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-01-28 08:50:25 +0300

    adding download functionality

---
+++ src/app/admin/products/[id]/download/route.ts
@@ -0,0 +1,21 @@
+import prisma from "@/db/db";
+import { NextResponse } from "next/server";
+import { notFound } from "next/navigation";
+import fs from "fs/promises";
+export async function GET(
+  req: NextResponse,
+  { params: { id } }: { params: { id: string } }
+) {
+  const product = await prisma.product.findUnique({ where: { id } });
+  if (product === null) notFound();
+  const { size } = await fs.stat(product.filePath);
+  const file = await fs.readFile(product.filePath);
+  const extension = product.filePath.split(".").pop();
+
+  return new NextResponse(file, {
+    headers: {
+      "Content-Dispositon": `attachement;filename="${product.name}.${extension}"`,
+      "Content-Length": size.toString(),
+    },
+  });
+}
