13af269dad1622a3e52208abd14805d53bdb553b
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-08-06 04:56:00 +0300

    mod: Update route.ts to use NextRequest instead of NextResponse

--- src/app/admin/products/[id]/download/route.ts
+++ src/app/admin/products/[id]/download/route.ts
@@ -1,9 +1,9 @@
 import prisma from "@/db/db";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { notFound } from "next/navigation";
 import fs from "fs/promises";
 export async function GET(
-  req: NextResponse,
+  req: NextRequest,
   { params: { id } }: { params: { id: string } }
 ) {
   const product = await prisma.product.findUnique({ where: { id } });
