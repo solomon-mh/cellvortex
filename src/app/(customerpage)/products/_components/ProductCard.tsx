673c910e7f3f31d8c74929fb786c32b10f3b91cc
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-08-06 12:03:50 +0300

    mod: modfying the products purchase link

--- src/app/(customerpage)/products/_components/ProductCard.tsx
+++ src/app/(customerpage)/products/_components/ProductCard.tsx
@@ -5,9 +5,10 @@ import {
   CardDescription,
   CardHeader,
 } from "@/components/ui/card";
-import { DropdownMenu } from "@/components/ui/dropdown-menu";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+
 type ProductCardProps = {
   card: {
     id: string;
@@ -21,21 +22,26 @@ type ProductCardProps = {
 
 const ProductCard: React.FC<ProductCardProps> = ({ card }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
+    <div className="grid grid-cols-2 lg:grid-cols-3 lg:w-3/4 mx-auto gap-6 p-4">
       {card.map((item) => (
-        <Card key={item.imagePath} className="border rounded-lg shadow-lg">
+        <Card
+          key={item.imagePath}
+          className="border rounded-lg shadow-lg flex flex-col"
+        >
           <CardHeader className="p-4 bg-gray-100">
             <h2 className="text-lg font-semibold">{item.name}</h2>
           </CardHeader>
-          <CardContent className="p-4">
-            <Image
-              src={item.imagePath}
-              alt={item.name}
-              width={400}
-              height={400}
-              className="w-full h-auto object-cover"
-            />
-            <CardDescription className="mt-4 text-gray-600">
+          <CardContent className="p-4 flex-grow flex flex-col">
+            <div className="relative w-full h-48 md:h-56 lg:h-64">
+              <Image
+                src={item.imagePath}
+                alt={item.name}
+                width={400}
+                height={400}
+                className="rounded"
+              />
+            </div>
+            <CardDescription className="mt-4 text-gray-600 flex-grow">
               {item.description}
             </CardDescription>
             <div className="flex justify-between items-center mt-4 font-bold text-lg">
@@ -44,13 +50,12 @@ const ProductCard: React.FC<ProductCardProps> = ({ card }) => {
                 ${(item.priceInCents / 100).toFixed(2)}
               </p>
             </div>
-            <a
-              href={`/products/${item.id}/download`}
-              download
-              className="mt-4 inline-block text-center w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition-colors duration-200"
+            <Link
+              href={`/products/${item.id}/purchase`}
+              className="mt-4 inline-block text-center w-full bg-lime-500 text-white font-bold py-2 rounded hover:bg-lime-600 transition-colors duration-200"
             >
-              Download
-            </a>
+              Purchase
+            </Link>
           </CardContent>
         </Card>
       ))}
