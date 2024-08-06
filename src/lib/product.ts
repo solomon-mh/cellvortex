50bf1187ca6c231cfd7de9857c3fa695894e84e1
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-08-06 11:58:04 +0300

    feat: adding route cache

--- src/lib/product.ts
+++ src/lib/product.ts
@@ -1,38 +1,48 @@
 import prisma from "@/db/db";
+import { cache } from "./cache";
+export const getLatestProducts = cache(
+  async () => {
+    const latestProducts = await prisma.product.findMany({
+      where: { isAvailableForPurchase: true },
+      select: {
+        id: true,
+        name: true,
+        priceInCents: true,
+        filePath: true,
+        imagePath: true,
+        description: true,
+      },
+      orderBy: { createdAt: "desc" },
+      take: 4,
+    });
+    // await new Promise((resolve) => setTimeout(resolve, 2000));
 
-export async function getLatestProduct() {
-  const latestProducts = prisma.product.findMany({
-    where: { isAvailableForPurchase: true },
-    select: {
-      id: true,
-      name: true,
-      priceInCents: true,
-      filePath: true,
-      imagePath: true,
-      description: true,
-    },
-    orderBy: { createdAt: "desc" },
-    take: 4,
-  });
+    return latestProducts;
+  },
+  ["/products", "getLatestProducts"],
+  { revalidate: (60 * 60 * 24) / 2 }
+);
+export const getPopularProducts = cache(
+  async () => {
+    const latestProducts = await prisma.product.findMany({
+      where: { isAvailableForPurchase: true },
+      select: {
+        id: true,
+        name: true,
+        priceInCents: true,
+        filePath: true,
+        imagePath: true,
+        description: true,
+      },
+      orderBy: {
+        orders: { _count: "desc" },
+      },
+      take: 4,
+    });
+    await new Promise((resolve) => setTimeout(resolve, 2000));
 
-  return latestProducts;
-}
-export async function getPopularProduct() {
-  const latestProducts = prisma.product.findMany({
-    where: { isAvailableForPurchase: true },
-    select: {
-      id: true,
-      name: true,
-      priceInCents: true,
-      filePath: true,
-      imagePath: true,
-      description: true,
-    },
-    orderBy: {
-      orders: { _count: "desc" },
-    },
-    take: 4,
-  });
-
-  return latestProducts;
-}
+    return latestProducts;
+  },
+  ["/products", "getPopularProducts"],
+  { revalidate: (60 * 60 * 24) / 2 }
+);
