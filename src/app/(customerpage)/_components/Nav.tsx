fb7e8f9cbfc70ae4bf0da06ad15434e937be8796
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-08-06 11:47:47 +0300

    mod: modfying the Navs logo

--- src/app/(customerpage)/_components/Nav.tsx
+++ src/app/(customerpage)/_components/Nav.tsx
@@ -1,14 +1,25 @@
-import { Nav, NavLink } from "@/components/Nav";
 import React from "react";
+import { Nav, NavLink } from "@/components/Nav";
+import Link from "next/link";
+import Image from "next/image";
 
 const Navigation = () => {
   return (
     <nav className="bg-gray-900 fixed top-0 w-full flex justify-end items-center shadow-lg z-50">
       <div className="logo absolute left-0 mx-4">
-        <div className="logo-img text-white">
-          <p className="text-4xl">Xp</p>
-        </div>
+        <Link href="/">
+          <div className="logo italic flex items-center text-gray-100">
+            <Image
+              src="/svgs/mobile-back.svg"
+              alt="logo"
+              width={35}
+              height={35}
+            />
+            <h3>cellvortex</h3>
+          </div>
+        </Link>
       </div>
+
       <>
         <Nav>
           <NavLink href="/">Home</NavLink>
